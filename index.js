7f36771eacd04d59b8f5a8c99ad8ab97f1ab22f3
Kyle Dallafior <dal@half.rest>
2016-10-07 14:39:45 +0200

    updating white and name of func

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var colors = require('colors');
 
 var myString = 'Hello World';
 
-function runInterval() {
+function makeRainbows() {
 
-console.log(myString);
+console.log(myString.white); // white
 console.log(myString.cyan); // cyan
 console.log(myString.blue); // blue
 console.log(myString.red); // red
 console.log(myString.yellow); // yellow
 }
 
-setInterval(runInterval, 5000);
+setInterval(makeRainbows, 1000);
