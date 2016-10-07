ac5badb4e6a9bdd1d2754a1167879c362a4986ab
Kyle Dallafior <dal@half.rest>
2016-10-07 13:44:36 +0200

    addjusting index.js - no theming needed

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 var colors = require('colors');
 
 
-colors.setTheme({
-  cyan: 'cyan',
-  blue: 'blue',
-  red: 'red',
-  yellow: 'yellow'
-});
-
 // console.log('OMG Rainbows!'.rainbow); // rainbow
 console.log('OMG Rainbows!'.cyan); // rainbow
 console.log('OMG Rainbows!'.blue); // rainbow
