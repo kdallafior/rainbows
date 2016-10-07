74a0991ce859be9bb961a0ad9d82bcde71f6d58f
Kyle Dallafior <dal@half.rest>
2016-10-07 13:34:08 +0200

    adding colors for separate lines in index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var colors = require('colors');
 
-console.log('OMG Rainbows!'.rainbow); // rainbow
+
+colors.setTheme({
+  cyan: 'cyan',
+  blue: 'blue',
+  red: 'red',
+  yellow: 'yellow'
+});
+
+// console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('OMG Rainbows!'.cyan); // rainbow
+console.log('OMG Rainbows!'.blue); // rainbow
+console.log('OMG Rainbows!'.red); // rainbow
+console.log('OMG Rainbows!'.yellow); // rainbow
